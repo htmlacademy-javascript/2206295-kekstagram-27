4fd7816ddc669ac53bccfac58a4f18574e4f7ccd
VladlenTar <114384254+VladlenTar@users.noreply.github.com>
2022-10-06 16:02:58 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-let minVlaue
-let maxValue
-let randomNumber
+let minVlaue;
+let maxValue;
+let randomNumber;
 let getRandomNumber = function (minVlaue, maxValue) {
   randomNumber = Math.floor(Math.random() * (maxValue - minVlaue + 1)) + minVlaue
   if (randomNumber < 0) {
-    randomNumber = NaN
-    return randomNumber
+    randomNumber = NaN;
+    return randomNumber;
   }
-  return randomNumber
+  return randomNumber;
 }
 
-let maxStringLength
-let lenght
-let acceptableLength
+let maxStringLength;
+let lenght;
+let acceptableLength;
 let checkLine = function (lenght, maxStringLength) {
   if (lenght <= maxStringLength) {
-    acceptableLength = true
+    acceptableLength = true;
   } else {
-    acceptableLength = false
+    acceptableLength = false;
   }
-  return acceptableLength
+  return acceptableLength;
 }
