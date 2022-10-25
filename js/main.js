61298a188243616c1fbab2c70129b2ac42d9e517
VladlenTar <114384254+VladlenTar@users.noreply.github.com>
2022-10-25 18:27:44 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
 const getRandomNumber = function (minVlaue, maxValue) {
-  let randomNumber = Math.floor(Math.random() * (maxValue - minVlaue + 1)) + minVlaue
+  let randomNumber = Math.floor(Math.random() * (maxValue - minVlaue + 1)) + minVlaue;
   if (randomNumber < 0) {
     randomNumber = NaN;
   }
   return randomNumber;
-}
+};
 
-const checkStringLength = function (lenght, maxStringLength) {
-  return lenght <= maxStringLength;
-}
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-const createRandomUser = function () {
-  const names = ['Даниил', 'Александр', 'Платон', 'Алиса', 'Софья', 'Агата', 'Максим', 'Полина', 'Анна', 'Марина', 'Ирина', 'Иван', 'Наталья', 'Дарья', 'Илья', 'Варвара', 'Герман', 'Анастасия', 'Елизавета', 'Амира', 'Камиль', 'Артём', 'Артур', 'Стефания', 'Зоя', 'Валерия', 'Михаил', 'Василий', 'Владислав', 'Алексей'];
-  const messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+const checkStringLength = function (length, maxStringLength) {
+  return length <= maxStringLength;
+};
+
+const NAMES = ['Даниил', 'Александр', 'Платон', 'Алиса', 'Софья', 'Агата', 'Максим', 'Полина', 'Анна', 'Марина', 'Ирина', 'Иван', 'Наталья', 'Дарья', 'Илья', 'Варвара', 'Герман', 'Анастасия', 'Елизавета', 'Амира', 'Камиль', 'Артём', 'Артур', 'Стефания', 'Зоя', 'Валерия', 'Михаил', 'Василий', 'Владислав', 'Алексей'];
+
+const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+
+const NUMBER_GENERATED_OBJECT = 25;
+
+const getIdList = function (maxListIndex) {
+  let list = [];
+  for (let i = 0; i < maxListIndex; i++) {
+    list.push(getRandomNumber(1, maxListIndex));
+  };
+  let list2 = Array.from(new Set(list));
+  list = list2;
+  while (list.length < maxListIndex) {
+    list.push(getRandomNumber(1, maxListIndex));
+    list2 = Array.from(new Set(list));
+    list = list2;
+  };
+  return list;
+};
+
+let idListPhoto = getIdList(NUMBER_GENERATED_OBJECT);
+
+let idListComment = getIdList(1000);
+
+const createRandomComment = function () {
   return {
-    id: getRandomNumber(1, 1000),
-    avatar: 'img/avatar' + getRandomNumber(1, 6) + '.svg',
-    message: messages[getRandomNumber(0, messages.length - 1)],
-    name: names[getRandomNumber(0, names.length - 1)],
+    id: idListComment.shift(),
+    avatar: `img/avatar${getRandomNumber(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
   }
-}
+};
+
+const createRandomPhoto = function () {
+  return {
+    id: idListPhoto.shift(),
+    url: `photos${getRandomNumber(1, NUMBER_GENERATED_OBJECT)}.jpg`,
+    description: 'Фотография',
+    likes: getRandomNumber(15, 200),
+    comments: createRandomComment(),
+  }
+};
+
+
+
+const dateForCheck = Array.from({ length: NUMBER_GENERATED_OBJECT }, createRandomPhoto);
 
